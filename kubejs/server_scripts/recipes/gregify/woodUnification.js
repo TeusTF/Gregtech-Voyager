6f1e28234fa7f0baf78f1f8a44364d3431e2f8be
TeusTF <207483626+TeusTF@users.noreply.github.com>
2026-04-26 19:47:52 -0500

    Updated woodUnification.js
    devs, please tag your woods.
    Fix for Vinery Woods

--- kubejs/server_scripts/recipes/gregify/woodUnification.js
+++ kubejs/server_scripts/recipes/gregify/woodUnification.js
@@ -1,84 +1,66 @@
-const cuttingfluid = [['lubricant','gtceu:lubricant 1',10],['distilled','gtceu:distilled_water 3',15],['water','minecraft:water 4',20]]
-//const hmah = Ingredient.of(/hexerei:.*mahogany.*/)
-//const hwil = Ingredient.of(/hexerei:.*willow.*/)
-//const hwhz = Ingredient.of(/hexerei:.*hazel.*/)
+const cuttingfluid = [['lubricant','gtceu:lubricant','1',10],['distilled','gtceu:distilled_water','3',15],['water','minecraft:water','4',20]]
 
-/*ServerEvents.tags('item', event => {
+ServerEvents.tags('item', event => {
 
 	//I hate it when mods don't tag things properly
-	const logs = event.get('minecraft:logs').getObjectIds()
-
-	logs.forEach(t =>{
-    	if (hmah.test(t)) event.add('hexerei:mahogany_logs',t)
-    	if (hwil.test(t)) event.add('hexerei:willow_logs',t)
-    	if (hwhz.test(t)) event.add('hexerei:hazel_logs',t)
-	})
-})*/
+	event.add('minecraft:oak_logs',['vinery:apple_log','vinery:apple_wood'])
+	event.add('vinery:dark_cherry_logs',['vinery:dark_cherry_log','vinery:dark_cherry_wood','vinery:stripped_dark_cherry_log','vinery:stripped_dark_cherry_wood'])
+	
+})
 
 
 
 ServerEvents.recipes(event => {
 
-	//event.remove({type:'minecraft:crafting_shapeless',output:'hexerei:mahogany_planks'})
-	//event.shapeless('2x hexerei:mahogany_planks', '#hexerei:mahogany_logs').id('hexerei:mahogany_planks')
-
-	event.forEachRecipe({ type: 'minecraft:crafting_shapeless', input:'#minecraft:logs', output: '#minecraft:planks'}, r => {
-		
-		let logtype = r.originalRecipeIngredients
-		let output = r.originalRecipeResult
-
-
-
-		event.shapeless(Item.of(output.id, 2), logtype[0])//.id(r.getId())
-
-		
-
-		event.recipes.gtceu.shaped((Item.of(output.id, 4)),
+	const sawShaped = (output,input,mod,itemName)=>{
+		let pref = ''
+		if (mod != 'minecraft') pref = mod+'_'
+		event.recipes.gtceu.shaped((Item.of(output, 4)),
 			[
 				'A',
 				'B'
 			],
 			{
 				A: '#forge:tools/saws',
-				B: logtype[0]
+				B: input
 			}
-		).id(`gtceu:${output.getMod()}_${output.getItem()}_saw`)
+		).id(`gtceu:shaped/${pref+itemName}_saw`)
+	}
 
+	const cuttingMachine = (output,input,custom,mod) => {
+		cuttingfluid.forEach(cuttingfluid => {
+			event.recipes.gtceu.cutter(`gtceu:cutter/${mod}_${custom}_${cuttingfluid[0]}`)
+				.itemInputs(input)
+				.inputFluids(cuttingfluid[1]+' '+cuttingfluid[2])
+				.itemOutputs(Item.of(output, 6))
+				.itemOutputs(Item.of('gtceu:wood_dust',2))
+				.EUt(7)
+				.duration(cuttingfluid[3]*20)
+			})
+	}
+
+	event.remove({type:'minecraft:crafting_shapeless',output:'minecraft:oak_planks',mod:'vinery'}) //Nuclear Option, This wouldn't work correctly the other ways I tried
+	event.remove({type:'minecraft:crafting_shapeless',output:'vinery:dark_cherry_planks'})
+
+	event.shapeless('2x vinery:dark_cherry_planks', '#vinery:dark_cherry_logs').id('vinery:dark_cherry_planks')
+	sawShaped('vinery:dark_cherry_planks','#vinery:dark_cherry_logs','vinery','dark_cherry_planks')
+	cuttingMachine('vinery:dark_cherry_planks','#vinery:dark_cherry_logs','dark_cherry_planks','vinery')
+
+	event.forEachRecipe({type: 'minecraft:crafting_shapeless', input:'#minecraft:logs', output: '#minecraft:planks'}, r => {
+		
+		let logtype = r.originalRecipeIngredients
+		let outputIS = r.originalRecipeResult
+		let output = outputIS.id
+		let mod = outputIS.getMod()
+		let itemName = outputIS.getItem()
+
+
+
+		event.shapeless(Item.of(outputIS.id, 2), logtype[0]).id(r.getId())
+		sawShaped(output,logtype[0],mod,itemName)
+		if (mod != 'minecraft') cuttingMachine(output,logtype[0],itemName,mod)
 
 
 	})
-	
-	//const logtype = ['fir','redwood','mahogany','jacaranda','palm','willow','dead','magic','umbran','hellbark']
-	//const cuttingfluid = [['lubricant','gtceu:lubricant 1',10],['distilled','gtceu:distilled_water 3',15],['water','minecraft:water 4',20]]
 
-	//event.remove({ output: [/biomesoplenty:.*_planks/]})
-
-
-/*	logtype.forEach(logtype => {
-
-		event.remove({type: 'minecraft:crafting_shapeless', output: `biomesoplenty:${logtype}_planks`})
-
-		event.shapeless(
-			`2x biomesoplenty:${logtype}_planks`,[`#biomesoplenty:${logtype}_logs`]
-		)
-		event.recipes.gtceu.shaped((`4x biomesoplenty:${logtype}_planks`),
-			[
-				'A',
-				'B'
-			],
-			{
-				A: '#forge:tools/saws',
-				B: `#biomesoplenty:${logtype}_logs`
-			}
-		)
-
-		cuttingfluid.forEach(cuttingfluid => {
-			event.recipes.gtceu.cutter(`kubejs:${logtype}_cutting_with_${cuttingfluid[0]}`)
-				.itemInputs(`#biomesoplenty:${logtype}_logs`)
-				.inputFluids(cuttingfluid[1])
-				.itemOutputs(`6x biomesoplenty:${logtype}_planks`)
-				.EUt(7)
-				.duration(cuttingfluid[2])
-		})
-	}) */
 })

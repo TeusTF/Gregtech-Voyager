f1e776a451ba0790a68823e635f79c9f9c0c5467
bzells <derpers1011@gmail.com>
2026-05-05 01:22:59 -0500

    hungry helper recipe

--- kubejs/server_scripts/recipes/multiblock/helper_calorie_conversion.js
+++ kubejs/server_scripts/recipes/multiblock/helper_calorie_conversion.js
@@ -2,29 +2,26 @@ ServerEvents.recipes(event => {
  
 
     event.shaped(
-        Item.of('gtceu:micro_farm', 1), // arg 1: output
+        Item.of('gtceu:power_rectangle_helper_calorie_converter', 1), // arg 1: output
         [
             'DCD',
             'BAB', // arg 2: the shape (array of strings)
             'DCD'
         ],
         {
-            A: 'gtceu:steel_machine_casing',
-            B: 'gtceu:treated_wood_frame',  //arg 3: the mapping object
-            C: '#gtceu:circuits/lv',
-            D: 'gtceu:tempered_glass'
+            A: 'gtceu:hv_helper_Wheel',
+            B: 'gtceu:refined_fluxed_electrum_frame',  //arg 3: the mapping object
+            C: '#gtceu:circuits/iv',
+            D: 'kubejs:radiation_proof_lead_casing'
         }
     )
 
 
-    event.recipes.gtceu.micro_farm("kubejs:radioactive_cookies")
+    event.recipes.gtceu.helper_calorie_conversion("kubejs:radioactive_cookies")
         .notConsumable("minecraft:sugar_cane")
-        .notConsumable("kubejs:farmer_helper")
+        .notConsumable(Item.of("kubejs:hungry_helper", '{conversionRate:1.5}'))
         .itemOutputs('minecraft:sugar_cane')
-        .circuit(2)
-        .chancedOutput('2x minecraft:sugar_cane', 9000, 1000)
-        .chancedOutput('4x minecraft:sugar_cane', 8000, 500)
         .inputFluids("minecraft:water 1000")
         .duration(100)
-        .EUt(2)
+        .EUt(-2)
 })
